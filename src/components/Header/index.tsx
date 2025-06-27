8184ef30e412c4df1999df82014522597cc5812e
Marianne Gomes <mariannegomesm@gmail.com>
2025-06-27 00:01:05 -0300

    fix: bold elements header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,8 +8,8 @@ import Logo from "../../../public/logo-circle.svg"
 
 const Header = ({ activeSection }: { activeSection: string }) => {
     return (
-        <header className="fixed top-0 left-0 right-0 z-50 flex justify-between px-[24px] lg:px-[82px] py-[18px] shadow-md bg-white">
-            <div className="flex">
+        <header className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-[24px] lg:px-[82px] py-[18px] shadow-md bg-white">
+            <div className="flex items-center">
                 <Image src={Logo} alt="Logo" className="mr-[12px]" />
                 <div className="flex flex-col">
                     <label className="font-medium whitespace-nowrap">Ring Strike</label>
@@ -17,14 +17,14 @@ const Header = ({ activeSection }: { activeSection: string }) => {
                 </div>
             </div>
             <div className="flex items-center gap-[72px]">
-                <ul className="hidden lg:flex gap-[32px]">
+                <ul className="hidden lg:flex gap-[28px]">
                     {menuOptions.map((item, index) => {
                         const isActive = activeSection === item.href.replace("#", "");
                         return (
                             <li key={`menu-${index}`} className="relative">
                                 <a
                                     href={item.href}
-                                    className={`font-light text-[14px] transition-colors ${isActive ? "text-[#BC090D]" : ""
+                                    className={`font-light text-[14px] transition-colors  ${isActive ? "text-[#BC090D] font-semibold" : ""
                                         }`}
                                 >
                                     {item.title}
